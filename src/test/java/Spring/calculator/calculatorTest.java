2d1ae69bfcc1faada1f583e251913182c6282d2f
naul2354 <conco2354@gmail.com>
2022-10-24 18:52:41 +0700

    add vui

--- src/test/java/Spring/calculator/calculatorTest.java
+++ src/test/java/Spring/calculator/calculatorTest.java
@@ -7,6 +7,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class calculatorTest {
     @Test
     public void testSum(){
-        assertEquals(5,calculator.sum(2,3));
+        assertEquals(2,calculator.sum(1,1));
     }
 }
